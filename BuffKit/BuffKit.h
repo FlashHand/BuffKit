c0e2611b3f35a65bba3cb22ac7782278b59bdaaa
Ral Wayne <ralwayne@163.com>
2016-05-19 16:58:38 +0800

    pod

--- BuffKit/BuffKit.h
+++ BuffKit/BuffKit.h
@@ -6,13 +6,9 @@
 //  Copyright © 2016年 BoWang. All rights reserved.
 //
 
-#ifndef _BUFFKIT_
-#define _BUFFKIT_
-
 #import "CryptoBuff.h"
 #import "LBSBuff.h"
 #import "NullBuff.h"
 #import "FrameBuff.h"
 #import "ColorBuff.h"
 
-#endif /* BuffKit_h */
