a2b9828365948dec0bb61a5f0ba2f4b745e6b0d7
FlashHand <ralwayne@163.com>
2016-06-07 13:26:27 +0800

    add rootVCSplitBuff

---
+++ BuffKit/NavigationBuff/RootVCSplitBuff.h
@@ -0,0 +1,13 @@
+//
+//  RootVCSplitBuff.h
+//  BuffDemo
+//
+//  Created by 王博 on 16/6/7.
+//  Copyright © 2016年 BoWang. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface RootVCSplitBuff : NSObject
+
+@end
---
+++ BuffKit/NavigationBuff/RootVCSplitBuff.m
@@ -0,0 +1,13 @@
+//
+//  RootVCSplitBuff.m
+//  BuffDemo
+//
+//  Created by 王博 on 16/6/7.
+//  Copyright © 2016年 BoWang. All rights reserved.
+//
+
+#import "RootVCSplitBuff.h"
+
+@implementation RootVCSplitBuff
+
+@end
