b3411c833ffcc2596672e1bc71b544023f8b072d
Ral Wayne <ralwayne@163.com>
2016-06-22 20:04:02 +0800

    pod

--- BuffKit/CellBuff/CellBuff.m
+++ BuffKit/CellBuff/CellBuff.m
@@ -4,8 +4,8 @@
 //
 
 #import "CellBuff.h"
-#import "FrameBuff.h"
 #import <objc/runtime.h>
+#import "FrameBuff.h"
 CGFloat _BF_GET_WIDTH(){
     CGFloat w =[FrameBuff screenWidth];
     CGFloat h =[FrameBuff screenHeight];
